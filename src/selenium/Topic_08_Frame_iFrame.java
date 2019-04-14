7f1161e5820db5031bf97b67ec884f9f41527192
ACER <ACER@192.168.1.9>
2019-04-14 15:44:56 +0700

    Removed unused library

--- src/selenium/Topic_08_Frame_iFrame.java
+++ src/selenium/Topic_08_Frame_iFrame.java
@@ -7,7 +7,6 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
